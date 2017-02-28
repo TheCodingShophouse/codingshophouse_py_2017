9ecc48154b35dcc398630886d16bbf305a13f129
DubstepCheesecake <kss_212@hotmail.com>
2017-02-28 21:26:14 +0800

    Created ForKey in models

--- coursegroup/course/models/topic.py
+++ coursegroup/course/models/topic.py
@@ -1,5 +1,7 @@
 from django.db import models
+from .course import Course
 
 class Topic(models.Model):
     title = models.CharField(max_length = 100)
     description = models.TextField()
+    course = models.ForeignKey(Course, null = True) 
